34bc6e43b63f15961a3a0e64d95071530e0c6937
jackbisceglia <jackbisceglia2000@gmail.com>
2021-01-19 19:18:21 -0500

    first commit

---
+++ sneakinderbackend/src/main/java/com/example/sneakinderbackend/SneakinderbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.sneakinderbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SneakinderbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SneakinderbackendApplication.class, args);
+	}
+
+}
---
+++ sneakinderbackend/src/test/java/com/example/sneakinderbackend/SneakinderbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sneakinderbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SneakinderbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
